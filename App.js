610828cbe17383c42b8221c79a6f0f028e4217ef
whvt <xm987654321@gmail.com>
2020-11-29 03:25:47 +0300

    change check on 0 todos (image)

--- App.js
+++ App.js
@@ -5,10 +5,9 @@ import { MainScreen } from './src/screens/MainScreen'
 import { TodoScreen } from './src/screens/TodoScreen'
 
 export default function App() {
-  const [todoId, setTodoId] = useState('2')
+  const [todoId, setTodoId] = useState(null)
   const [todos, setTodos] = useState([
-    { id: '1', title: 'Выучить React Native' },
-    { id: '2', title: 'Написать приложение' },
+    // { id: '1', title: 'Выучить React Native' }
   ])
 
   const addTodo = (title) => {
