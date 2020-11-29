e9499d1ba6f1892471eb9435f104c79e33490cd8
whvt <xm987654321@gmail.com>
2020-11-30 01:38:22 +0300

    add expo-font

--- App.js
+++ App.js
@@ -3,6 +3,14 @@ import { StyleSheet, Text, View, FlatList, Alert } from 'react-native'
 import { Navbar } from './src/components/Navbar'
 import { MainScreen } from './src/screens/MainScreen'
 import { TodoScreen } from './src/screens/TodoScreen'
+import * as Font from 'expo-font'
+
+async function loadApplication() {
+  await Font.loadAsync({
+    'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+  })
+}
 
 export default function App() {
   const [todoId, setTodoId] = useState(null)
